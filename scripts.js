baf0d84002f39eafb1bd7c40b541c55ec9cdcc85
CuesAlt <devinsliferalt@gmail.com>
2024-09-18 14:52:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 document.querySelectorAll('.add-to-cart').forEach(button => {
     button.addEventListener('click', (e) => {
         const product = e.target.dataset.product;
-        alert(`${product} has been added to your cart.`);
+        alert(`${product} has been added to your cart!`);
+    });
+});
+
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
